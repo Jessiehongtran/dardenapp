7b562970b30e5179d4e2aada20f55641e9b5a5d3
Your Name <you@example.com>
2020-01-09 14:48:06 -0800

    refractored data structure at home page

---
+++ client/src/data/serviceCategory.js
@@ -0,0 +1,31 @@
+export const serviceCategories = 
+[
+    {
+        name: "Cleaning",
+        icon: "https://res.cloudinary.com/dfulxq7so/image/upload/c_scale,h_219/v1578439023/d47c5362a369f945d1edd6ca13c9afb4_kckp3h.png"
+    },
+    {
+        name: "Hair Cut",
+        icon: "https://res.cloudinary.com/dfulxq7so/image/upload/v1578439038/39c5df853a7f2b4dad3c239424fe1b2e_wegodu.png"
+    },
+    {
+        name: "Personal Trainer",
+        icon: "https://res.cloudinary.com/dfulxq7so/image/upload/v1578439057/7f1f21c35daf03b671ba301b9e2c973e_1_ejxzz9.png"
+    },
+    {
+        name: "Moving Buddy",
+        icon: "https://res.cloudinary.com/dfulxq7so/image/upload/v1578439080/a6b93c33ec7ec5f7823b894c9119cf38_erts1v.png"
+    },
+    {
+        name: "Laundry",
+        icon: "https://res.cloudinary.com/dfulxq7so/image/upload/v1578439103/ae6d39b555d61c8cb6135bd5e7bc75d7_kuaaha.png"
+    },
+    {
+        name: "Pet Sitting/Walking",
+        icon: "https://res.cloudinary.com/dfulxq7so/image/upload/v1578439122/c21ea7036eaef219e81ab364d6e622f3_ld1ryl.png"
+    },
+    {
+        name: "Anything Else",
+        icon: "https://res.cloudinary.com/dfulxq7so/image/upload/v1578439142/0e5cd02a978baa406c0f279ffdd57000_vlticr.png"
+    },
+]
