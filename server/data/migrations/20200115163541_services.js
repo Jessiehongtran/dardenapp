923595efcf33149b8b94b414ad936e21bd3f4ea3
Your Name <you@example.com>
2020-01-16 14:21:07 -0800

    edit migration of services

--- server/data/migrations/20200115163541_services.js
+++ server/data/migrations/20200115163541_services.js
@@ -3,6 +3,7 @@ exports.up = function(knex) {
   return knex.schema.createTable("services", column => {
     column.increments();
     column.string("service_name").notNullable();
+    column.string("service_icon")
   })
 };
 
