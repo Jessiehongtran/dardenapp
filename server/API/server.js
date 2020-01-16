44a138a24bf21b99de60923609e3ee11dfc1ed14
Your Name <you@example.com>
2020-01-15 21:35:14 -0800

    done routing

--- server/API/server.js
+++ server/API/server.js
@@ -2,8 +2,20 @@ const express = require('express');
 
 const server = express();
 
+const clientRouter = require('./user_client/client.router');
+const servicesRouter = require('./services/services.router');
+const requestRouter = require('./request_client/request.router');
+
+server.use(express.json());
+server.use('/api/client', clientRouter);
+server.use('/api/services', servicesRouter);
+server.use('/api/request', requestRouter);
+
+
+
 server.get('/', (req, res) => {
     res.send('Hello World')
 })
+
 
 module.exports = server;
