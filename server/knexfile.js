8df82fdd9a18087e755d2fc9ca674a6b276bcbb0
Your Name <you@example.com>
2020-01-16 11:15:54 -0800

    commit

--- server/knexfile.js
+++ server/knexfile.js
@@ -24,12 +24,7 @@ module.exports = {
 
   production: {
     client: "pg",
-    connection: {
-      host: process.env.DB_HOST,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASS,
-      database: process.env.DB_NAME
-    },
+    connection: process.env.DATABASE_URL,
     migrations: {
       directory: "./data/migrations"
     },
