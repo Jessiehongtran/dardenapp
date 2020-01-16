c51a0ea139b48a4c16348a62bf31c4a442324fe1
Your Name <you@example.com>
2020-01-16 12:27:19 -0800

    migrated for postgres

--- server/index.js
+++ server/index.js
@@ -1,23 +1,6 @@
 require('dotenv').config();
 const server = require('./API/server');
 
-const { Client } = require('pg');
-
-const client = new Client({
-  connectionString: process.env.DATABASE_URL,
-  ssl: true,
-});
-
-client.connect();
-
-client.query('SELECT * FROM database_file.db3;', (err, res) => {
-  if (err) throw err;
-  for (let row of res.rows) {
-    console.log(JSON.stringify(row));
-  }
-  client.end();
-});
-
 const PORT = process.env.PORT ;
 
 server.listen(PORT, () => {
