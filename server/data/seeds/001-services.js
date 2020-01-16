f2010c320c5e00e44e1e98dc7c69066f6b8f396d
Your Name <you@example.com>
2020-01-16 12:36:31 -0800

    migrated postgres successfully , having problem with truncate in seeding but not important now

--- server/data/seeds/001-services.js
+++ server/data/seeds/001-services.js
@@ -1,7 +1,7 @@
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('services').truncate()
+  return knex('services').del()
     .then(function () {
       // Inserts seed entries
       return knex('services').insert([
--- server/data/seeds/002-user_client.js
+++ server/data/seeds/002-user_client.js
@@ -1,7 +1,7 @@
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('user_client').truncate()
+  return knex('user_client').del()
     .then(function () {
       // Inserts seed entries
       return knex('user_client').insert([
