f9958931d605ba793e2ebc5dea5d5225cb5deb92
Your Name <you@example.com>
2020-01-16 11:11:02 -0800

    fixed PORT

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const server = require('./API/server');
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT ;
 
 server.listen(PORT, () => {
     console.log(`Server running on port ${PORT}...`)
