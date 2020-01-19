a9ca1cc8fcb489ba0925a3cf8125e17410583b7f
Your Name <you@example.com>
2020-01-19 14:43:52 -0800

    fixed GET requests to get all info from user, service, booking, not yet payment

--- server/API/request_client/request.model.js
+++ server/API/request_client/request.model.js
@@ -2,7 +2,17 @@ const db = require('../../data/dbConfig');
 
 
 function findAll(){
-    return db("request_client");
+    return db("request_client")
+            .join("services", "services.id", "request_client.service_id")
+            .join("user_client", "user_client.id", "request_client.user_id")
+            .select(
+                "user_client.email", 
+                "services.service_name", 
+                "request_client.unit",
+                "request_client.hours",
+                "request_client.address",
+                "request_client.created_at"
+                )
 }
 
 function addRequest(request){
