4d770d1d769ccfa85a2c4cfe56173c2d5fc47a3c
Your Name <you@example.com>
2020-01-19 22:01:31 -0800

    test

--- server/API/request_client/request.model.js
+++ server/API/request_client/request.model.js
@@ -25,17 +25,29 @@ function addRequest(request){
 
 function getRequestById(id){
     return db("request_client")
+            .select(
+                "user_client.email", 
+                "services.service_name", 
+                "request_client.unit",
+                "request_client.hours",
+                "request_client.address",
+                "request_client.created_at"
+            )
+            .where("request_client.id", id)
+            .join("services", "services.id", "request_client.service_id")
+            .join("user_client", "user_client.id", "request_client.user_id")
+           
             // .join("services", "services.id", "request_client.service_id")
             // .join("user_client", "user_client.id", "request_client.user_id")
-            // .select([
+            // .select(
             //     "user_client.email", 
             //     "services.service_name", 
             //     "request_client.unit",
             //     "request_client.hours",
             //     "request_client.address",
             //     "request_client.created_at"
-            // ])
-            .where("request_client.id", id)
+            // )
+            // .where("request_client.id", id)
             
 }
 
